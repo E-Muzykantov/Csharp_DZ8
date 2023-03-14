5f0733ff407dc53f12a755d7e9395bdba4ec8d97
Egor Muzykantov <linemail@mail.ru>
2023-03-14 05:03:49 +0300

    Решение задачи 54

---
+++ Ex54/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+Console.Clear();
+int[,] array = FillArray(3, 4, 0, 10);
+PrintArray(array);
+
+SortElementFormUpToLowInRowsArray(array);
+Console.WriteLine();
+PrintArray(array);
+
+
+int[,] FillArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write("{0,5}\t", inputArray[i, j]);
+        }
+        Console.WriteLine("");
+    }
+}
+
+void SortElementFormUpToLowInRowsArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int m = 0; m < array.GetLength(1) - 1; m++)
+            {
+                if (array[i, m] < array[i, m + 1])
+                {
+                    int temp = array[i, m + 1];
+                    array[i, m + 1] = array[i, m];
+                    array[i, m] = temp;
+                }
+            }
+        }
+    }
+}
